bcbcc463f14bbdea4695114b846179a871c81098
Andrea Greco <andrea.greco@wise.com>
2023-08-25 13:13:39 +0200

    second exercise

---
+++ src/main/java/tech/andreagreco/Message.java
@@ -0,0 +1,16 @@
+package tech.andreagreco;
+
+public class Message {
+
+    String address;
+    String timestamp;
+    String type;
+
+    public Message() {
+        super();
+    }
+
+    public String getAdress() {
+        return this.address;
+    }
+}
